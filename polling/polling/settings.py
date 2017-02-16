8a8f0cbcd1b54effa2a0198f9c866dbcdecf6731
William Barnes <barnesw91291@gmail.com>
2017-02-16 14:30:59 -0800

    basic route and view

--- polling/polling/settings.py
+++ polling/polling/settings.py
@@ -75,8 +75,7 @@ WSGI_APPLICATION = 'polling.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': '',
     }
 }
 
