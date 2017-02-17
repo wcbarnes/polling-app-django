769c3f23c320d065749e2693dc1199a62c3150ac
William Barnes <barnesw91291@gmail.com>
2017-02-17 10:37:06 -0800

    rendering a question template at root route

--- polling/polls/views.py
+++ polling/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 import mongoengine
@@ -7,6 +8,17 @@ from polls.models import Poll, Choice
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Poll.objects().all()
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 
