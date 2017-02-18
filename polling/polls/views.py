4634d7bf4b765aa15ef21ebd19f0e23edb23a40c
William Barnes <barnesw91291@gmail.com>
2017-02-18 12:29:42 -0800

    can now create polling questions on the homepage

--- polling/polls/views.py
+++ polling/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.utils import timezone
 
 # Create your views here.
 import mongoengine
@@ -11,6 +12,18 @@ from polls.models import Poll, Choice
 def index(request):
     latest_question_list = Poll.objects().all()
     context = {'latest_question_list': latest_question_list}
+    try:
+        question = Poll(question=request.POST['question'], pub_date=timezone.now())
+        choices = []
+        for i in range(4):
+            if len(request.POST['answer' + str(i + 1)]) > 0:
+                choices.append(request.POST['answer' + str(i + 1)])
+        for choice in choices:
+            answer = Choice(choice_text=choice)
+            question.choices.append(answer)
+        question.save()
+    except:
+        print "NOT A POST REQUEST"
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
@@ -33,11 +46,10 @@ def vote(request, question_id):
     except Poll.DoesNotExist:
         raise Http404("Question does not exist")
     try:
-        selected_choice = question.choices
         for choice in question.choices:
             if choice.uid == int(request.POST['choice']):
                 selected_choice = choice
-    except (KeyError):
+    except KeyError:
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
